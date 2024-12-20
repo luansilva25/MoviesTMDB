fafcd359519f8e3583d6aa3740fb05f486f6edcf
luansilva25 <luansilva250807@gmail.com>
2024-12-20 16:44:08 -0300

    json accepting

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,8 @@ const tokenAccess = import.meta.env.VITE_TOKEN_ACCESS
 const api = axios.create({
     baseURL: `https://api.themoviedb.org/3`,
     headers: {
-        Authorization: `Bearer ${tokenAccess}`
+        Authorization: `Bearer ${tokenAccess}`,
+        Accept: 'application/json'
     }
 })
 
